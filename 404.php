57149acc55e3a7708ff9a0a334f8379c8b61f302
Surbma <peter@surbma.hu>
2021-06-20 01:07:12 +0200

    10.0
    - 404 template synced with official version.
    - Removed header.php an footer.php, as there is no customizations in these files anymore.

--- 404.php
+++ 404.php
@@ -3,14 +3,23 @@
 <div id="main-content">
 	<div class="container">
 		<div id="content-area" class="clearfix">
-			<article id="post-0" <?php post_class( 'et_pb_post not_found' ); ?>>
-				<?php
-					if ( is_active_sidebar( 'divi-plus-404' ) ) { dynamic_sidebar( 'divi-plus-404' ); }
-					else { get_template_part( 'includes/no-results', '404' ); }
-				?>
-			</article> <!-- .et_pb_post -->
+			<div id="left-area">
+				<article id="post-0" <?php post_class( 'et_pb_post not_found' ); ?>>
+					<?php
+						if ( is_active_sidebar( 'divi-plus-404' ) ) {
+							dynamic_sidebar( 'divi-plus-404' );
+						} else {
+							get_template_part( 'includes/no-results', '404' );
+						}
+					?>
+				</article> <!-- .et_pb_post -->
+			</div> <!-- #left-area -->
+
+			<?php get_sidebar(); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
 </div> <!-- #main-content -->
 
-<?php get_footer(); ?>
+<?php
+
+get_footer();
